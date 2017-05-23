4ce3661c2b69ad59377b696a737dfae2355f3721
nardosm <nardosmgrs@gmail.com>
2017-05-23 19:49:08 -0400

    Update imdbInfo.js

--- imdbInfo.js
+++ imdbInfo.js
@@ -1,6 +1,42 @@
 var myConnector = mstr.createDataConnector();
     
     // Download the data
+    myConnector.fetchTable = function(table, params, doneCallback) {};
+
+    mstr.validateDataConnector(myConnector);
+
+    // Create event listeners for when the user submits the form
+    $(document).ready(function() {
+        $("#submitButton").click(function() {
+            console.log("Customer click submit");
+            mstr.connectionData = {}; // Use this variable to pass data to your fetchTable functions
+            mstr.connectionName = "RawDataJson"; // This will be the data source name in mstr
+            mstr.fileType = 'JSON';
+            var params = {};
+            mstr.connectionData = mstr.connectionData;
+            mstr.authenticationInfo = "";
+            mstr.authType = mstr.authTypeEnum.anonymous;
+            mstr.tableList = [];
+            mstr.tableList.push({tableName: "RawDataJson12"});
+            console.log(JSON.stringify(params));
+	    mstr.fetchURL = "http://rawgit.com/nardosm/omdb-connector/master/test.json"
+            window.mstr.submit();
+            //window.close();
+        });
+    });
+//})();
+
+
+
+
+
+
+
+
+/*
+var myConnector = mstr.createDataConnector();
+    
+    // Download the data
     myConnector.fetchTable = function(table, params, doneCallback) {
         var mstrObj = JSON.parse(params);
         var url = "http://rawgit.com/nardosm/omdb-connector/master/test.json";
@@ -32,3 +68,4 @@ var myConnector = mstr.createDataConnector();
         });
     });
 //})();
+*/
